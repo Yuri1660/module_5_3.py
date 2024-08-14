98517410b7f8502e7d8e07b555ff0c4cb1ceafd7
Yuri1660 <iyra42rus@gmail.com>
2024-08-14 10:32:21 +0700

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,72 @@
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.floors = floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.floors == other.floors
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.floors < other.floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.floors <= other.floors
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.floors > other.floors
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.floors >= other.floors
+        return NotImplemented
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.floors += value
+            return self
+        return NotImplemented
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)
+
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+
+h1 += 10
+print(h1)
+
+h2 = 10 + h2
+print(h2)
+
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
